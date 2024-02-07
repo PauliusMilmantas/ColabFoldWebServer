a4fa753b33babe00cfe3385149af693a8779aade
Paulius Milmantas <pauliusmilmantas@Pauliuss-Mini.lan>
2024-02-07 16:07:30 +0200

    Restructuring

--- Worker/src/app.module.ts
+++ Worker/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RabbitMqController } from './rabbitmq.controller';
+import { MMseqs2Service } from './services/MMseqs2/MMseqs2.service';
 
 @Module({
-  controllers: [RabbitMqController]
+  controllers: [RabbitMqController],
+  providers: [
+    MMseqs2Service
+  ]
 })
 export class AppModule {}
